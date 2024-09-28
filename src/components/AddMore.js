45299ab83259ee6292d2843be98086d7d234dddb
Qajay0832 <qajay0832@gmail.com>
2024-09-29 02:28:36 +0530

    first commit

---
+++ src/components/AddMore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddMore = () => {
+  return (
+    <div>
+        <input type='text' id='name'></input>
+        <input type='number' id='age'></input>
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default AddMore
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fields, setFields] = useState([{ name: '', age: '' }]);
+  const AddField=()=>{
+    setFields([...fields,{ name: '', age: '' }])
+  }
+  const removeField=(index)=>{
+    const newField=fields.filter((_,i)=>i!==index);
+    setFields(newField)
+  }
+  const handleName=(index,event)=>{
+    const newField=[...fields];
+    newField[index].name=event.target.value;
+    setFields(newField);
+  }
+  const handleAge=(index,event)=>{
+    const newField=[...fields];
+    newField[index].age=event.target.value;
+    setFields(newField);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(fields);
+};
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+
+        {fields.map((field, index) => (
+          <div key={index} className="field-container">
+            <input type='text' id='name' placeholder="Name" value={field.name} onChange={(e)=>{handleName(index,e)}} required></input>
+            <input type='number' id='age' placeholder="Age" value={field.age} onChange={(e)=>{handleAge(index,e)}} required></input>
+            <button type="button" onClick={()=>{removeField(index)}}>Remove</button>
+          </div>
+        ))}
+        <button type="button" onClick={()=>{AddField()}}>Add More..</button>
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   )
 }
